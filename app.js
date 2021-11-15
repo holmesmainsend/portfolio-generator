21325dcdda0703d8e6f74fc8f8d89b861f389400
Mona Lisa <holmesmainsend@gmail.com>
2021-11-14 21:29:51 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require("inquirer");
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name1, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name1, github), err => {
-    if (err) throw err;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(name1, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
